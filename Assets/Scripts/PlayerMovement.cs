8e149555a10ee436e66e1500f36fb13ab5a635a5
Parsa KamaliPour <parsakamalipour@gmail.com>
2020-08-11 19:49:07 +0430

    part 1 done

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private CharacterController _characterController;
+    private Animator _animator;
+
+    [SerializeField]
+    private float _moveSpeed = 100;
+    [SerializeField]
+    private float _turnSpeed = 5f;
+
+    private void Awake()
+    {
+        _characterController = GetComponent<CharacterController>();
+        _animator = GetComponentInChildren<Animator>();
+    }
+
+    private void Update()
+    {
+        var horizontal = Input.GetAxis("Horizontal");
+        var vertical = Input.GetAxis("Vertical");
+
+        var movement = new Vector3(horizontal, 0, vertical);
+        _characterController.SimpleMove(movement * Time.deltaTime * _moveSpeed);
+
+        _animator.SetFloat("Speed", movement.magnitude);
+
+        if (movement.magnitude > 0)
+        {
+            Quaternion newDirection = Quaternion.LookRotation(movement);
+            transform.rotation = Quaternion.Slerp(transform.rotation, newDirection, Time.deltaTime * _turnSpeed);
+        }
+
+    }
+}
