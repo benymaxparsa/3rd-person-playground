9f56146bc7da73b4fd018962178e96e02fb5b410
Parsa KamaliPour <parsakamalipour@gmail.com>
2020-08-17 19:12:07 +0430

    player movement and animations added

---
+++ Assets/Demo/Scripts/PlayerMovement1.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+
+public class PlayerMovement1 : MonoBehaviour
+{
+    private CharacterController characterController;
+    private Animator animator;
+
+    [SerializeField]
+    private float forwardMoveSpeed = 7.5f;
+
+    [SerializeField]
+    private float backwardMoveSpeed = 3;
+
+    [SerializeField]
+    private float turnSpeed = 150f;
+
+    private void Awake()
+    {
+        characterController = GetComponent<CharacterController>();
+        animator = GetComponentInChildren<Animator>();
+
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    private void Update()
+    {
+        var horizontal = Input.GetAxis("Mouse X");
+        var vertical = Input.GetAxis("Vertical");
+
+
+        animator.SetFloat("Speed", vertical);
+
+        transform.Rotate(Vector3.up, horizontal * turnSpeed * Time.deltaTime);
+
+        if (vertical != 0)
+        {
+            float moveSpeedToUse = vertical > 0 ? forwardMoveSpeed : backwardMoveSpeed;
+            characterController.SimpleMove(transform.forward * moveSpeedToUse * vertical);
+        }
+
+    }
+}
---
+++ Assets/One of a Kind/Scripts/PlayerMovement.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private CharacterController characterController;
+    private Animator animator;
+
+    [SerializeField]
+    private float turnSpeed = 150f;
+    [SerializeField]
+    private float backwardMoveSpeed = 3f;
+    [SerializeField]
+    private float forwardMoveSpeed = 7.5f;
+
+    private void Awake()
+    {
+        characterController = GetComponent<CharacterController>();
+        animator = GetComponentInChildren<Animator>();
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    private void Update()
+    {
+        var horizontal = Input.GetAxis("Mouse X");
+        var vertical = Input.GetAxis("Vertical");
+
+        animator.SetFloat("Speed", vertical);
+
+        transform.Rotate(Vector3.up, horizontal * turnSpeed * Time.deltaTime);
+
+        if (vertical != 0)
+        {
+            float moveSpeedToUse = vertical > 0 ? forwardMoveSpeed : backwardMoveSpeed;
+            characterController.SimpleMove(transform.forward * moveSpeedToUse * vertical);
+        }
+    }
+
+}
